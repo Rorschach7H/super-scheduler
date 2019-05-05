43febf13fb83026e7af805b292517a19ec861898
meixiaoxi <meixiaoxi110@gmail.com>
2019-05-05 18:03:28 +0800

    引入spring的crom解析逻辑代码，待优化。。。

---
+++ mxx-scheduler-core/src/main/java/com/meixiaoxi/scheduler/cron/CronTrigger.java
@@ -0,0 +1,55 @@
+package com.meixiaoxi.scheduler.cron;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+public class CronTrigger implements Trigger {
+
+    private final CronSequenceGenerator sequenceGenerator;
+
+
+    public CronTrigger(String expression) {
+        this.sequenceGenerator = new CronSequenceGenerator(expression);
+    }
+
+    public CronTrigger(String expression, TimeZone timeZone) {
+        this.sequenceGenerator = new CronSequenceGenerator(expression, timeZone);
+    }
+
+    public String getExpression() {
+        return this.sequenceGenerator.getExpression();
+    }
+
+
+    @Override
+    public Date nextExecutionTime(TriggerContext triggerContext) {
+        Date date = triggerContext.lastCompletionTime();
+        if (date != null) {
+            Date scheduled = triggerContext.lastScheduledExecutionTime();
+            if (scheduled != null && date.before(scheduled)) {
+                date = scheduled;
+            }
+        } else {
+            date = new Date();
+        }
+        return this.sequenceGenerator.next(date);
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        return (this == other || (other instanceof CronTrigger &&
+                this.sequenceGenerator.equals(((CronTrigger) other).sequenceGenerator)));
+    }
+
+    @Override
+    public int hashCode() {
+        return this.sequenceGenerator.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.sequenceGenerator.toString();
+    }
+
+}
